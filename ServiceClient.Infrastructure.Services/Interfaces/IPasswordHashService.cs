4e43d0b77a25fe004302bb6ac2d93758bc0de2a9
Vinby <48126197+Vinby@users.noreply.github.com>
2021-12-31 13:00:59 +0400

    password hasher

---
+++ ServiceClient.Infrastructure.Services/Interfaces/IPasswordHashService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceClient.Infrastructure.Services.Interfaces
+{
+    public interface IPasswordHashService
+    {
+        string Hash(string password);
+    }
+}
---
+++ ServiceClient.Infrastructure.Services/PasswordHashService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceClient.Infrastructure.Services
+{
+  
+
+    //https://github.com/dotnet/AspNetCore/blob/main/src/Identity/Extensions.Core/src/PasswordHasher.cs
+    public class PasswordHashService : IPasswordHashService
+    {
+        private const int _iterCount = 10_000;
+
+        public string Hash(string password)
+        {
+            return Convert.ToBase64String(HashPasswordV3(password, RandomNumberGenerator.Create()));
+        }
+
+
+        private byte[] HashPasswordV3(string password, RandomNumberGenerator rng)
+        {
+            return HashPasswordV3(password, rng,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterCount: _iterCount,
+                saltSize: 128 / 8,
+                numBytesRequested: 256 / 8);
+        }
+
+        private byte[] HashPasswordV3(string password, RandomNumberGenerator rng, KeyDerivationPrf prf, int iterCount, int saltSize, int numBytesRequested)
+        {
+            // Produce a version 3 (see comment above) text hash.
+            byte[] salt = new byte[saltSize];
+            rng.GetBytes(salt);
+            byte[] subkey = KeyDerivation.Pbkdf2(password, salt, prf, iterCount, numBytesRequested);
+
+            var outputBytes = new byte[13 + salt.Length + subkey.Length];
+            outputBytes[0] = 0x01; // format marker
+            WriteNetworkByteOrder(outputBytes, 1, (uint)prf);
+            WriteNetworkByteOrder(outputBytes, 5, (uint)iterCount);
+            WriteNetworkByteOrder(outputBytes, 9, (uint)saltSize);
+            Buffer.BlockCopy(salt, 0, outputBytes, 13, salt.Length);
+            Buffer.BlockCopy(subkey, 0, outputBytes, 13 + saltSize, subkey.Length);
+            return outputBytes;
+        }
+
+        private static void WriteNetworkByteOrder(byte[] buffer, int offset, uint value)
+        {
+            buffer[offset + 0] = (byte)(value >> 24);
+            buffer[offset + 1] = (byte)(value >> 16);
+            buffer[offset + 2] = (byte)(value >> 8);
+            buffer[offset + 3] = (byte)(value >> 0);
+        }
+    }
+
+
+}
